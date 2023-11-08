a2a2a8118a356c69423b537a329237568cc3b136
GuiZucyszyn <131690384+GuiZucyszyn@users.noreply.github.com>
2023-11-08 16:22:39 -0300

    Update install_anydesk.sh

--- install_anydesk.sh
+++ install_anydesk.sh
@@ -6,7 +6,7 @@ sudo apt remove --purge -y anydesk*
 mkdir /tmp/Anydesk
 
 # URL para o arquivo .deb do AnyDesk
-wget -q https://my.anydesk.com/v2/api/v2/custom-clients/downloads/public/EYSVU8MKZAC4/anydesk-rentcars-infra-6.3.0-1_amd64.deb -P /tmp/Anydesk
+wget -q https://my.anydesk.com/v2/api/v2/custom-clients/downloads/public/YNUEB2E4PPUY/anydesk-client-rentcars-6.3.0-1_amd64.deb -P /tmp/Anydesk
 
 
 # Instala o AnyDesk usando o dpkg
